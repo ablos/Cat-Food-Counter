d53fa40799516c511ab2f9fd1ab731364f213fd1
Abel Dieterich <dieterich.abel@gmail.com>
2025-05-28 14:08:12 +0200

    Improved circuit, voltage divider can now be turned off to increase battery life significantly

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <Adafruit_SSD1306.h>
 
 // Memory data
-struct RTCData 
+struct Memory 
 {
     uint32_t crc32;
     uint32_t lastWakeTime;
@@ -15,6 +15,7 @@ struct RTCData
 // Defenitions
 #define MULTI_PRESS_WINDOW 200 // In ms
 #define OLED_POWER_PIN D4
+#define VDIV_ENABLE_PIN D5
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -23,8 +24,8 @@ struct RTCData
 
 // Function declarations
 uint32_t calculateCRC32(const uint8_t *data, size_t length);
-bool readRTCData(RTCData* data);
-void writeRTCData(RTCData* data);
+bool readMemory(Memory* data);
+void writeMemory(Memory* data);
 void decideAction(uint8_t pressCount);
 void checkBatteryVoltage();
 void wakeDisplay();
@@ -34,8 +35,12 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup()
 {
-    RTCData data;
-    bool validData = readRTCData(&data);
+    system_phy_set_powerup_option(1);  // Minimal RF during boot
+    WiFi.mode(WIFI_OFF);               // Explicit WiFi disable
+    WiFi.forceSleepBegin();            // Force radio sleep
+
+    Memory data;
+    bool validData = readMemory(&data);
     uint32_t currentTime = millis();
     
     // Check if this wake is within multi-press window and update press count accordingly
@@ -46,7 +51,7 @@ void setup()
 
     // Save the updated count
     data.lastWakeTime = currentTime;
-    writeRTCData(&data);
+    writeMemory(&data);
 
     Serial.begin(115200);
     Serial.printf("\n\nPress count: %d\n", data.pressCount);
@@ -57,7 +62,7 @@ void setup()
     // No more presses came, so reset presscount and execute action
     uint8_t pressCount = data.pressCount;
     data.pressCount = 0;
-    writeRTCData(&data);
+    writeMemory(&data);
     decideAction(pressCount);
 
     // Return to deep sleep
@@ -97,14 +102,24 @@ void decideAction(uint8_t pressCount)
 
 void checkBatteryVoltage() 
 {
+    // Enable voltage divider
+    pinMode(VDIV_ENABLE_PIN, OUTPUT);
+    digitalWrite(VDIV_ENABLE_PIN, HIGH);
+    delay(10);
+
+    // Set up analog pin
     pinMode(A0, INPUT);
-            
+    
+    // Read and convert voltage
     int adcValue = analogRead(A0);
     float adcVoltage = (adcValue / 1024.0) * 3.25;
     float batteryVoltage = adcVoltage * ((330.0 + 680.0) / 680.0);
 
     Serial.println(adcVoltage);
     Serial.println(batteryVoltage);
+    
+    // Disable voltage divider
+    digitalWrite(VDIV_ENABLE_PIN, LOW);
 }
 
 void wakeDisplay() 
@@ -133,18 +148,25 @@ void wakeDisplay()
         // Put on display
         display.display();
     }
+}
 
-    delay(5000);
+void turnOffDisplay() 
+{
+    // Shut down display
+    display.ssd1306_command(SSD1306_DISPLAYOFF);
+    display.clearDisplay();
+    delay(10);
 
     // Power down OLED screen
+    pinMode(OLED_POWER_PIN, OUTPUT);
     digitalWrite(OLED_POWER_PIN, LOW);
 }
 
-bool readRTCData(RTCData* data) 
+bool readMemory(Memory* data) 
 {
-    if (ESP.rtcUserMemoryRead(0, (uint32_t*)data, sizeof(RTCData))) 
+    if (ESP.rtcUserMemoryRead(0, (uint32_t*)data, sizeof(Memory))) 
     {
-        uint32_t crcOfData = calculateCRC32((uint8_t *)&data->lastWakeTime, sizeof(RTCData) - sizeof(data->crc32));
+        uint32_t crcOfData = calculateCRC32((uint8_t *)&data->lastWakeTime, sizeof(Memory) - sizeof(data->crc32));
         
         if (crcOfData == data->crc32)
             return true;
@@ -156,10 +178,10 @@ bool readRTCData(RTCData* data)
     return false;
 }
 
-void writeRTCData(RTCData* data) 
+void writeMemory(Memory* data) 
 {
-    data->crc32 = calculateCRC32((uint8_t *)&data->lastWakeTime, sizeof(RTCData) - sizeof(data->crc32));
-    ESP.rtcUserMemoryWrite(0, (uint32_t *)data, sizeof(RTCData));
+    data->crc32 = calculateCRC32((uint8_t *)&data->lastWakeTime, sizeof(Memory) - sizeof(data->crc32));
+    ESP.rtcUserMemoryWrite(0, (uint32_t *)data, sizeof(Memory));
 }
 
 uint32_t calculateCRC32(const uint8_t *data, size_t length) 
