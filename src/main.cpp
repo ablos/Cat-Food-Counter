49ccd4d0c5c0f755898942b48ae7aea55cd566b2
Abel Dieterich <dieterich.abel@gmail.com>
2025-06-04 14:57:36 +0200

    Implemented MQTT functionality

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,35 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <PubSubClient.h>
+#include "config.h"
 
 // Structs
-struct FeedingTime 
+struct FeedingMoment 
 {
-    uint8_t hour;
-    uint8_t minute;
-    
-    String toString() 
+    uint32_t dateTimeValue;
+
+    String dateString() 
     {
-        char buffer[6];
-        sprintf(buffer, "%02d:%02d", hour, minute);
+        uint16_t date = dateTimeValue / 10000;
+        
+        if (date == 9999)
+            return "?\?-?\?";
+
+        char buffer[7];
+        sprintf(buffer, "%02d-%02d", date / 100, date % 100);
+        return String(buffer);
+    }
+    
+    String timeString() 
+    {
+        uint16_t time = dateTimeValue % 10000;
+        
+        if (time == 9999)
+            return "?\?:?\?";
+
+        char buffer[7];
+        sprintf(buffer, "%02d:%02d", time / 100, time % 100);
         return String(buffer);
     }
 };
@@ -22,11 +40,10 @@ struct Memory
 {
     uint32_t crc32;
     uint32_t lastWakeTime;
-    uint8_t pressCount;
-    uint8_t padding1;
-    uint16_t feedings[4];
+    uint32_t feedings[4];
     uint8_t feedingCount;
-    uint8_t padding2;
+    uint8_t pressCount;
+    uint16_t padding;       // Extra padding for memory alignment
 };
 
 // Defenitions
@@ -38,28 +55,35 @@ struct Memory
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
-#define SCREEN_WAKE_TIME 5000
 
 // Function declarations
 uint32_t calculateCRC32(const uint8_t *data, size_t length);
 bool readMemory(Memory* data);
 void writeMemory(Memory* data);
 void decideAction(uint8_t pressCount);
-void checkBatteryVoltage();
+float getBatteryVoltage();
 void wakeDisplay();
 void turnOffDisplay();
 void updateDisplay();
 bool isDisplayOn();
 void waitForDisplayOff();
-void addFeeding(uint16_t timeValue);
-uint16_t getLatestFeeding();
-void removeLatestFeeding();
-void clearAllFeedings();
+void addFeedingToMemory(uint32_t dateTimeValue);
+uint32_t getLatestFeedingFromMemory();
+void removeLatestFeedingFromMemory();
+void clearAllFeedingsFromMemory();
+bool connectMqtt();
+void mqttCallback(char* topic, byte* payload, unsigned int length);
+void addFeeding();
+void removeFeeding();
 
 // Variables
 Memory memoryData;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 unsigned long displayStartTime = 0;
+WiFiClient wifi;
+PubSubClient mqtt(wifi);
+uint32_t currentDateTime = 99999999;
+float currentBatteryVoltage = 0;
 
 void setup()
 {
@@ -101,27 +125,36 @@ void setup()
 
 void loop() {}
 
+// Decides an action based on press count
 void decideAction(uint8_t pressCount) 
 {
-    // Decide action based on press count
     switch (pressCount) 
     {
         // Handle single press
         case 1: 
             Serial.println("Single press detected");
-            checkBatteryVoltage();
+            Serial.println(getBatteryVoltage());
             break;
 
         // Handle double press
         case 2:
             Serial.println("Double press detected");
+            addFeeding();
             break;
-            
+
         // Handle triple press
         case 3:
             Serial.println("Triple press detected");
+            removeFeeding();
             break;
-            
+
+        // Handle quadriple press
+        case 4:
+            Serial.println("Quadriple press detected");
+            clearAllFeedingsFromMemory();
+            updateDisplay();
+            break;
+
         // Default behaviour
         default:
             Serial.println("Too many presses - treating as single");
@@ -129,28 +162,39 @@ void decideAction(uint8_t pressCount)
     }
 }
 
-void checkBatteryVoltage() 
+// Checks the battery voltage
+float getBatteryVoltage() 
 {
     // Enable voltage divider
     pinMode(VDIV_ENABLE_PIN, OUTPUT);
     digitalWrite(VDIV_ENABLE_PIN, HIGH);
-    delay(10);
+    delay(50);
 
     // Set up analog pin
     pinMode(A0, INPUT);
     
-    // Read and convert voltage
-    int adcValue = analogRead(A0);
-    float adcVoltage = (adcValue / 1024.0) * 3.25;
-    float batteryVoltage = adcVoltage * ((330.0 + 680.0) / 680.0);
+    // Take multiple readings and average them
+    const int numReadings = 10;
+    int totalReading = 0;
 
-    Serial.println(adcVoltage);
-    Serial.println(batteryVoltage);
+    for (int i = 0; i < numReadings; i++) 
+    {
+        totalReading += analogRead(A0);
+        delay(10);
+    }
+
+    // Read and convert voltage
+    int adcValue = totalReading / numReadings;
+    float adcVoltage = (adcValue / 1024.0) * MCP_OUTPUT_VOLTAGE;
+    float batteryVoltage = adcVoltage * ((330.0 + 680.0) / 680.0);
     
     // Disable voltage divider
     digitalWrite(VDIV_ENABLE_PIN, LOW);
+
+    return batteryVoltage + VOLTAGE_OFFSET;
 }
 
+// Turns the display on after giving power with the transistor
 void wakeDisplay() 
 {
     // Power on OLED screen
@@ -165,6 +209,7 @@ void wakeDisplay()
     updateDisplay();
 }
 
+// Turns the display off and cuts power with transistor
 void turnOffDisplay() 
 {
     // Shut down display
@@ -175,8 +220,11 @@ void turnOffDisplay()
     // Power down OLED screen
     pinMode(OLED_POWER_PIN, OUTPUT);
     digitalWrite(OLED_POWER_PIN, LOW);
+
+    Serial.println("Display turned off");
 }
 
+// Updates the display information
 void updateDisplay() 
 {
     // Clear buffer
@@ -187,8 +235,14 @@ void updateDisplay()
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
     
+    // Retrieve latest feeding
+    FeedingMoment latestMoment;
+    latestMoment.dateTimeValue = getLatestFeedingFromMemory();
+
     // Print text
-    display.println("Hello world!");
+    display.println("Latest feeding date: " + latestMoment.dateString());
+    display.println("Latest feeding time: " + latestMoment.timeString());
+    display.println("Current feeding count: " + String(memoryData.feedingCount));
 
     // Put on display
     display.display();
@@ -197,6 +251,7 @@ void updateDisplay()
     displayStartTime = millis();
 }
 
+// Checks if the display is on, if it is on for more than the wake time, turn it off
 bool isDisplayOn() 
 {
     if (millis() - displayStartTime > SCREEN_WAKE_TIME) 
@@ -208,6 +263,7 @@ bool isDisplayOn()
     return true;
 }
 
+// Wait until the display is off
 void waitForDisplayOff() 
 {
     while (isDisplayOn()) 
@@ -216,7 +272,8 @@ void waitForDisplayOff()
     }
 }
 
-void addFeeding(uint16_t timeValue) 
+// Add a new feeding moment
+void addFeedingToMemory(uint32_t dateTimeValue) 
 {
     if (memoryData.feedingCount >= 4)
         return;
@@ -226,22 +283,24 @@ void addFeeding(uint16_t timeValue)
         memoryData.feedings[i] = memoryData.feedings[i - 1];
     
     // Add new feedings at front
-    memoryData.feedings[0] = timeValue;
+    memoryData.feedings[0] = dateTimeValue;
     if (memoryData.feedingCount < 4)
         memoryData.feedingCount++;
 
     writeMemory(&memoryData);
 }
 
-uint16_t getLatestFeeding() 
+// Retrieve the latest feeding moment
+uint32_t getLatestFeedingFromMemory() 
 {
     if (memoryData.feedingCount > 0)
         return memoryData.feedings[0];
 
-    return 0;
+    return 99999999;
 }
 
-void removeLatestFeeding() 
+// Remove the latest feeding moment
+void removeLatestFeedingFromMemory() 
 {
     if (memoryData.feedingCount == 0)
         return;
@@ -256,7 +315,8 @@ void removeLatestFeeding()
     writeMemory(&memoryData);
 }
 
-void clearAllFeedings() 
+// Clears all feeding records
+void clearAllFeedingsFromMemory() 
 {
     memoryData.feedingCount = 0;
 
@@ -266,6 +326,151 @@ void clearAllFeedings()
     writeMemory(&memoryData);
 }
 
+// Connects to WiFi and MQTT
+bool connectMqtt() 
+{
+    Serial.println("Connecting to MQTT...");
+    // First read battery voltage, since WiFi can create noise on the analog input
+    currentBatteryVoltage = getBatteryVoltage();
+
+    // Enable WiFi
+    WiFi.forceSleepWake();
+    WiFi.mode(WIFI_STA);
+    
+    // Configure static IP if provided
+    if (strlen(STATIC_IP) > 0) 
+    {
+        IPAddress ip, gateway, subnet, dns;
+        ip.fromString(STATIC_IP);
+        gateway.fromString(GATEWAY_IP);
+        subnet.fromString(SUBNET_MASK);
+        dns.fromString(DNS_SERVER);
+
+        WiFi.config(ip, gateway, subnet, dns);
+    }
+
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+    // Wait for connection with 10 sec timeout
+    unsigned long start = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - start < 10000)
+        delay(10);
+        
+    // If we failed to connect after timeout return
+    if (WiFi.status() != WL_CONNECTED)
+        return false;
+        
+    // Set up MQTT
+    mqtt.setServer(MQTT_SERVER, MQTT_PORT);
+    mqtt.setCallback(mqttCallback);
+    
+    // Connect to MQTT (with authentication if present)
+    bool connected = false;
+    
+    if (strlen(MQTT_USER) > 0)
+        connected = mqtt.connect(MQTT_NAME, MQTT_USER, MQTT_PASS);
+    else
+        connected = mqtt.connect(MQTT_NAME);
+
+    Serial.println("Connection successfull: " + String(connected));
+
+    return connected;
+}
+
+// Disconnects from MQTT and WiFi
+void disconnectMqtt() 
+{
+    mqtt.disconnect();
+    WiFi.disconnect();
+    WiFi.mode(WIFI_OFF);
+    WiFi.forceSleepBegin();
+}
+
+// This is called when a MQTT message is received
+void mqttCallback(char* topic, byte* payload, unsigned int length) 
+{
+    // We receive the current time, parse it and store it
+    currentDateTime = 0;
+    for (unsigned int i = 0; i < length; i++)
+    {
+        if (payload[i] >= '0' && payload[i] <= '9')
+            currentDateTime = currentDateTime * 10 + (payload[i] - '0');
+    }
+
+    Serial.printf("Received datetime: %d\n", currentDateTime);
+}
+
+// Sends the latest data over MQTT
+void sendUpdate() 
+{
+    if (mqtt.connected()) 
+    {
+        Serial.println("Sending update...");
+
+        String json = "{\"count\":" + String(memoryData.feedingCount) + ", \"datetime\":" + String(getLatestFeedingFromMemory()) + ", \"battery-voltage\":" + String(currentBatteryVoltage) + "}";
+
+        mqtt.publish(MQTT_SEND, json.c_str());
+    }
+}
+
+// Adds a feeding moment, both synced to MQTT and to memory
+void addFeeding() 
+{
+    Serial.println("Adding feeding...");
+
+    // Try to connect to MQTT
+    bool connected = connectMqtt();
+    if (connected)
+    {
+        // Subscribe to the time topic
+        mqtt.subscribe(MQTT_RECV);
+        
+        // Wait for the retained message with 3 sec timeout
+        unsigned long start = millis();
+        while (millis() - start < 3000 && currentDateTime == 99999999) 
+        {
+            mqtt.loop();
+            yield();
+        }
+    }
+    
+    // Check if current date is still as last feeding date, if not reset feedings
+    if (memoryData.feedingCount > 0 && currentDateTime / 10000 != getLatestFeedingFromMemory() / 10000)
+        clearAllFeedingsFromMemory();
+
+    // Add feeding with current time to memory
+    addFeedingToMemory(currentDateTime);
+    
+    // Update the display
+    updateDisplay();
+    
+    // Send update over MQTT
+    sendUpdate();
+
+    // Disconnect MQTT
+    disconnectMqtt();
+}
+
+// Removes the last feeding moment both from MQTT and memory
+void removeFeeding() 
+{
+    Serial.println("Removing feeding...");
+
+    // Remove the latest feeding from memory
+    removeLatestFeedingFromMemory();
+    
+    // Update the display
+    updateDisplay();
+    
+    // Try to connect to MQTT and send update
+    if (connectMqtt())
+        sendUpdate();
+        
+    // Disconnect MQTT
+    disconnectMqtt();
+}
+
+// Reads RTC memory
 bool readMemory(Memory* data) 
 {
     if (ESP.rtcUserMemoryRead(0, (uint32_t*)data, sizeof(Memory))) 
@@ -282,12 +487,14 @@ bool readMemory(Memory* data)
     return false;
 }
 
+// Writes RTC memory
 void writeMemory(Memory* data) 
 {
     data->crc32 = calculateCRC32((uint8_t *)&data->lastWakeTime, sizeof(Memory) - sizeof(data->crc32));
     ESP.rtcUserMemoryWrite(0, (uint32_t *)data, sizeof(Memory));
 }
 
+// Calculates CRC for data validity
 uint32_t calculateCRC32(const uint8_t *data, size_t length) 
 {
     uint32_t crc = 0xffffffff;
